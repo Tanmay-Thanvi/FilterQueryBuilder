150412ea918fbbc45512ae5c5900d2f4c9b9e963
Tanmay Thanvi <78643125+Tanmay-Thanvi@users.noreply.github.com>
2024-11-13 16:24:42 +0530

    required changes in main file for Second Approach

--- SecondApproach/QueryBuilderMain.java
+++ SecondApproach/QueryBuilderMain.java
@@ -1,42 +1,37 @@
 package SecondApproach;
 
-import FirstApproach.Filter;
-import FirstApproach.FilterCriteria;
-import FirstApproach.Operator;
-import FirstApproach.*;
-
 import java.util.List;
 
 public class QueryBuilderMain {
 
     public static void main(String[] args) {
-        FirstApproach.Filter filter = new FirstApproach.Filter(
+        Filter filter = new Filter(
             List.of(
-                new FirstApproach.FilterCriteria("alertEntity", FirstApproach.Operator.EQ, "PATIENT_PLANNER"),
-                new FirstApproach.FilterCriteria("createDateCrieria", FirstApproach.Operator.OR,
+                new FilterCriteria("alertEntity", Operator.EQ, "PATIENT_PLANNER"),
+                new FilterCriteria("createDateCrieria", Operator.OR,
                     List.of(
-                        new FirstApproach.FilterCriteria("createDate", FirstApproach.Operator.LTE, "31 Aug"),
+                        new FilterCriteria("createDate", Operator.LTE, "31 Aug"),
                         new FilterCriteria("createDate", Operator.IS_NULL, null)
                     )
                 )
             )
         );
 
-        FilterTreeBuilder filterTreeBuilder = new FilterTreeBuilder();
-        Filter customFilter = filterTreeBuilder.preprocessFilter(filter);
-
-        if (customFilter != null) {
-            FilterTreeNode root = filterTreeBuilder.buildParseTree(customFilter.getFilterCriterias().get(0));
-
-            System.out.println("Root : " + root);
-
-            // Print Tree Traversal
-            System.out.println("Filter Parse Tree : ");
-            root.printTree(0);
-
-            System.out.println("Sql statement : " + root.getSqlStatement());
-        } else {
-            System.out.println("Error : Invalid Filter");
-        }
+        //        FilterTreeBuilder filterTreeBuilder = new FilterTreeBuilder();
+        //        Filter customFilter = filterTreeBuilder.preprocessFilter(filter);
+        //
+        //        if (customFilter != null) {
+        //            FilterTreeNode root = filterTreeBuilder.buildParseTree(customFilter.getFilterCriterias().get(0));
+        //
+        //            System.out.println("Root : " + root);
+        //
+        //            // Print Tree Traversal
+        //            System.out.println("Filter Parse Tree : ");
+        //            root.printTree(0);
+        //
+        //            System.out.println("Sql statement : " + root.getSqlStatement());
+        //        } else {
+        //            System.out.println("Error : Invalid Filter");
+        //        }
     }
 }
